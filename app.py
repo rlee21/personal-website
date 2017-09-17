3193d2118e348f6cfac309916abaf699d4d520d9
relee2 <relee@avvo.com>
2017-09-16 21:13:26 -0700

    add base, index, experience templates along with bootstrap files

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'hello world'
+    return render_template('index.html')
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
 
 
 if __name__ == '__main__':
