9b32649dcbe608cd946890a93a4a43612c496606
rlee21 <rlee21@comcast.net>
2017-09-29 02:07:42 -0700

    rewrite from scratch

--- app.py
+++ app.py
@@ -13,16 +13,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/experience')
-def experience():
-    return render_template('experience.html')
-
-
-@app.route('/resume')
-def resume():
-    return render_template('resume.html')
-
-
 if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
